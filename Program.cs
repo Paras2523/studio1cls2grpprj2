548cc1e77367647316624c4f0b86326b78da4257
Joshua Smith (1000125321) <SMITJA21@student.op.ac.nz>
2024-05-16 15:25:35 +1200

    Files added

--- Program.cs
+++ Program.cs
@@ -17,8 +17,43 @@ namespace ConsoleApp2
             pow = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"your answer is {Math.Pow(num, pow)}");
         }
+
+        public static void josh()
+        {
+            int num1, num2, num3;
+            string temp;
+
+            Console.Write("Please enter your first number: ");
+            temp = Console.ReadLine();
+            num1 = Convert.ToInt32(temp);
+            Console.Write("Please enter your second number: ");
+            temp = Console.ReadLine();
+            num2 = Convert.ToInt32(temp);
+            Console.Write("Please enter your third number: ");
+            temp = Console.ReadLine();
+            num3 = Convert.ToInt32(temp);
+
+            if ((num1 <= num2) && (num2 <= num3))
+
+            {
+                Console.WriteLine("Your numbers are in ascending numerical order");
+            }
+            else
+
+            {
+
+                Console.WriteLine("Your numbers are not in an ascending numerical order");
+
+            }
+
+            Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
+        
+
         }
+
     }
 }
