761a131b6bcb32d5d777e88b8b7d6a338f870141
Paras Paras (1000113338) <PARA12@student.op.ac.nz>
2024-05-16 15:23:02 +1200

    cls2

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        public static void paras()
+        {
+            int num, pow;
+            Console.WriteLine("enter the number.");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the ammount of power.");
+            pow = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"your answer is {Math.Pow(num, pow)}");
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
