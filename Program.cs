bee004c0839312aed5af8814434596e0b50c030a
Paras Paras (1000113338) <PARA12@student.op.ac.nz>
2024-05-16 15:30:40 +1200

    paras2

--- Program.cs
+++ Program.cs
@@ -17,6 +17,15 @@ namespace ConsoleApp2
             pow = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"your answer is {Math.Pow(num, pow)}");
         }
+        public static void paras2()
+        {
+            int num, pow;
+            Console.WriteLine("enter the number.");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the ammount of power.");
+            pow = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"your answer is {Math.Pow(num, pow)}");
+        }
         static void Main(string[] args)
         {
         }
